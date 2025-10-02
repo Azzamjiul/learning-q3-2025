96d42536203054272cc73c31ff5c64825060b86f
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2025-10-02 07:00:57 +0700

    feat: basic memory

---
+++ 2-memory/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 2-memory!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2-memory/src/01_mem_basic.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+def main():
+    messages = []
+
+    while True:
+        user_input = input("User: ")
+        messages.append({"role": "user", "content": user_input})
+        response = client.chat.completions.create(
+            model="gpt-4.1-nano-2025-04-14",
+            messages=messages,
+        )
+        answer = response.choices[0].message
+        messages.append(answer)
+        print(answer.content)
+
+        print("----" * 20)
+        print(messages)
+        print("----" * 20)
+
+
+if __name__ == "__main__":
+    main()
